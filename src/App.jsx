e19f3386de5714f17712b80460e0ec8810eed640
hattori100000 <clownlaugh100@gmail.com>
2024-06-27 22:56:05 +0545

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+<main className='relative'>
+  Nav
+<section className='xl:padding-1 wide:padding-r padding-b'>
+  Hero
+
+  <section className='padding'>
+PopularProduct
+  </section>
+  <section className='padding'>
+SuperQuality
+  </section>
+
+  <section className='padding'>
+Services
+  </section>
+  <section className='padding'>
+SpecialOffer
+  </section>
+  <section className='bg-pale-blue padding'>
+Customer reviews
+  </section>
+
+  <section className='padding-x sm:py-32 py-16 w-full'>
+Subscribe 
+  </section>
+
+  <section className='bg-black padding-x padding-t pb-8'>
+Footer
+  </section>
+
+
+</section>
+
+</main>
+    </>
+  )
+}
+
+export default App
